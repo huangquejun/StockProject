c6057c816f2a562e906dfc0babc3d22b132bc455
huangquejun <junhuangque@163.com>
2016-09-23 23:24:38 +0800

    9月23日

--- src/testFile.py
+++ src/testFile.py
@@ -3,16 +3,25 @@ Created on 2016年9月23日
 
 @author: Administrator
 '''
-import urllib.request
-import json
-html = urllib.request.urlopen(r'http://shuorel.com/ws/share/valuation/v1/601398?date=2016-3-5')
-data = html.read().decode('utf-8')
-print(data)
-jsondata = json.loads(data)
-print(jsondata['data'])
-print(jsondata.keys())
-print(len(jsondata['data']))
-print(type(jsondata['data']))
-for num in jsondata['data']:
-    print(num['code'])
-# [1]['pe_ratio']
+import tushare as ts
+import pymysql
+import pandas as pd
+from sqlalchemy.types import String
+from sqlalchemy import create_engine
+pymysql.install_as_MySQLdb()
+#读取股票基本数据
+engine = create_engine('mysql://root:iloveyou@127.0.0.1/stockdata?charset=utf8',echo=True)
+stockData = pd.read_sql('SELECT * FROM stockdata.stock_basics where timeToMarket<>0;', engine)
+stockData = stockData.set_index('code')
+
+for stockCode in stockData.index:
+    stockTime = stockData['timeToMarket'][stockCode]
+    print(type(stockTime))
+    break
+# df = ts.get_h_data('000001','2016-09-01','2016-09-23')
+# df['code'] = '000001'
+# df = df.set_index([df.index,df.code])
+# del df['code']
+# engine = create_engine('mysql://root:iloveyou@127.0.0.1/stockdata?charset=utf8',echo=True)
+# df.to_sql('stock_history',engine,if_exists='append',dtype={'code': String(10)})
+# # print(df)
--- src/urlRequest.py
+++ src/urlRequest.py
@@ -6,11 +6,11 @@ Created on 2016年9月19日
 '''
 import urllib.request
 import json
+import pandas as pd
 url = 'http://shuorel.com/ws/share/valuation/v1/601398?dates=2015-3-5,2016-3-5'
 stdout=urllib.request.urlopen(url)
 htmlCode = stdout.read().decode('utf-8')
-print(htmlCode.splitlines())
 jsonData = json.loads(htmlCode)
-
-print(type(jsonData['data']))
-print(jsonData['data'])
+df = pd.DataFrame(jsonData['data'])
+df = df.set_index(['code','date'])
+print(df)
