caad45e2d287d60c29905938137798ca57a1beec
huangquejun <junhuangque@163.com>
2016-09-25 22:41:29 +0800

    lastest 2016-09-25

--- src/crawlStockHistory.py
+++ src/crawlStockHistory.py
@@ -14,19 +14,21 @@ pymysql.install_as_MySQLdb()
 engine = create_engine('mysql://root:iloveyou@127.0.0.1/stockdata?charset=utf8')
 stockData = pd.read_sql('SELECT * FROM stockdata.stock_basics where timeToMarket<>0;', engine)
 stockData = stockData.set_index('code')
-engine.execute("drop table stock_history;")
+# engine.execute("drop table stock_history;")
+dfHistory = pd.read_sql('SELECT code,min(date) time FROM stockdata.stock_history group by code;', engine)
+dfHistory = dfHistory.set_index('code')
 loop = 0
 for stockCode in stockData.index:   
-    if loop == 2: break
     print(loop)
     tmp =stockData['timeToMarket'][stockCode]
     timeArray = time.strptime(tmp, "%Y%m%d")
     stockDate = time.strftime("%Y-%m-%d", timeArray)
-    loop+= 1
     print(stockCode)
     print(stockDate)
+    if stockCode in dfHistory.index : continue
     df = ts.get_h_data(stockCode,stockDate,'2016-09-23',drop_factor = False)
     df['code'] = stockCode
     df = df.set_index([df.index,df.code])
     del df['code']
     df.to_sql('stock_history',engine,if_exists='append',dtype={'code': String(10),'date': String(20)})
+    loop+= 1
--- src/test.py
+++ src/test.py
@@ -13,14 +13,15 @@ pymysql.install_as_MySQLdb()
 engine = create_engine('mysql://root:iloveyou@127.0.0.1/stockdata?charset=utf8')
 dfHistory = pd.read_sql('SELECT code,min(date) time FROM stockdata.stock_history group by code;', engine)
 dfHistory = dfHistory.set_index('code')
-stockData = pd.read_sql('SELECT * FROM stockdata.stock_basics where timeToMarket<>0;', engine)
-stockData = stockData.set_index('code')
-
-for code in dfHistory.index:
-    tmpDate = dfHistory['time'][code]
-    print(tmpDate)
-    earlyDate = tmpDate[0:10]
-    print(earlyDate)
-    tmp =stockData['timeToMarket'][code]
-    timeArray = time.strptime(tmp, "%Y%m%d")
-    stockDate = time.strftime("%Y-%m-%d", timeArray)
+print(dfHistory)
+# stockData = pd.read_sql('SELECT * FROM stockdata.stock_basics where timeToMarket<>0;', engine)
+# stockData = stockData.set_index('code')
+# 
+# for code in dfHistory.index:
+#     tmpDate = dfHistory['time'][code]
+#     print(tmpDate)
+#     earlyDate = tmpDate[0:10]
+#     print(earlyDate)
+#     tmp =stockData['timeToMarket'][code]
+#     timeArray = time.strptime(tmp, "%Y%m%d")
+#     stockDate = time.strftime("%Y-%m-%d", timeArray)
