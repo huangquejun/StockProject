91fd3af75c2be9481d42b5a1356d5a9f66499b73
Administrator <Administrator@2013-20150714IH>
2016-09-26 17:38:23 +0800

    在单位update

--- src/crawlIndexData.py
+++ src/crawlIndexData.py
@@ -5,8 +5,12 @@ Created on 2016年9月26日
 '''
 import tushare as ts
 import time
-now = time.strftime('%H-%M-%S',time.localtime(time.time()))
-print(now)
+
+# now = time.strftime("%H:%M:%S")
+# nowtime = time.strptime(now,'%H:%M:%S')
+
+tmpnow = '9:35:15'
+now = time.strptime(tmpnow,'%H:%M:%S')
 tmpMoningS = '9:30:00' 
 tmpMoningE = '11:30:00' 
 tmpNoonS = '13:00:00' 
@@ -15,6 +19,14 @@ MoningS = time.strptime(tmpMoningS,'%H:%M:%S')
 MoningE = time.strptime(tmpMoningE,'%H:%M:%S')
 NoonS = time.strptime(tmpNoonS,'%H:%M:%S')
 NoonE = time.strptime(tmpNoonE,'%H:%M:%S')
-print(MoningS)
-# df = ts.get_index()
-# print(df)
+while True:
+    if(now > NoonE):
+        print('now > NoonE')
+        break
+    if(now < MoningS or (now >MoningE and now <MoningE)):
+        print('second tiaojian')
+        continue
+    print('交易时间')
+    time.sleep(5)
+     
+
