48d55e47036f94ab5ca23a1f70641446a9624f91
Administrator <Administrator@2013-20150714IH>
2016-09-28 11:17:42 +0800

    new python file

--- src/DatabaseCon.py
+++ src/DatabaseCon.py
@@ -10,6 +10,6 @@ from sqlalchemy.types import String
 from sqlalchemy import create_engine
 pymysql.install_as_MySQLdb()
 df=ts.get_stock_basics()
-engine = create_engine('mysql://root:iloveyou@127.0.0.1/world?charset=utf8',echo=True)
+engine = create_engine('mysql://root:iloveyou@127.0.0.1/stockdata?charset=utf8',echo=True)
 df.to_sql('stock_basics',engine,if_exists='replace',dtype={'code': String(10),'timeToMarket': String(10)})
 engine.execute("update stock_basics set name=replace(name,' ','');")
