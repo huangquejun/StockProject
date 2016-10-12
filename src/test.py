a2a5318a5e907759b98b62747a932ce166b23d22
Administrator <Administrator@2013-20150714IH>
2016-10-12 17:35:51 +0800

    version update

--- src/test.py
+++ src/test.py
@@ -2,34 +2,33 @@
 Created on 2016年9月24日
 
 @author: Administrator
+
 '''
 import tushare as ts
-import pandas as pd
-from sqlalchemy import create_engine
 import pymysql
 import time
+import os
+import pandas as pd
+from sqlalchemy.types import String
+from sqlalchemy import create_engine
 pymysql.install_as_MySQLdb()
 
-# engine = create_engine('mysql://root:iloveyou@127.0.0.1/stockdata?charset=utf8')
-# dfHistory = pd.read_sql('SELECT code,min(date) time FROM stockdata.stock_history group by code;', engine)
-# dfHistory = dfHistory.set_index('code')
-# print(dfHistory)
-# stockData = pd.read_sql('SELECT * FROM stockdata.stock_basics where timeToMarket<>0;', engine)
-# stockData = stockData.set_index('code')
-# 
-# for code in dfHistory.index:
-#     tmpDate = dfHistory['time'][code]
-#     print(tmpDate)
-#     earlyDate = tmpDate[0:10]
-#     print(earlyDate)
-#     tmp =stockData['timeToMarket'][code]
-#     timeArray = time.strptime(tmp, "%Y%m%d")
-#     stockDate = time.strftime("%Y-%m-%d", timeArray)
+def createStockData(stockCode,timeToMarket,nowUpdateTime):
+    pass
 
-df1 = ts.get_h_data('000001',start='2016-09-26', end='2016-09-26', index=True)
-df2 = ts.get_h_data('000001', start='2016-09-23', end='2016-09-23',index=True)
-df3 = ts.get_h_data('000001', start='2016-09-23', end='2016-09-23',index=True)
-tmp = [df1,df2,df3]
-result = pd.concat(tmp)
-result = result.drop_duplicates()
-print(result)
+def updateStockDate(stockCode,lastUpdateTime,nowUpdateTime):
+    pass
+
+def getAndStoreLatestStockData():
+    stockBasicData=ts.get_stock_basics()
+    engine = create_engine('mysql://root:iloveyou@127.0.0.1/stockdata?charset=utf8',echo=True)
+    stockBasicData.to_sql('stock_basics',engine,if_exists='replace',dtype={'code': String(10),'timeToMarket': String(10)})
+    engine.execute("update stock_basics set name=replace(name,' ','');")
+    return stockBasicData
+
+def getStockDataBookMark():
+    pass
+
+if __name__ == "__main__":
+    print(getAndStoreLatestStockData())
+   
